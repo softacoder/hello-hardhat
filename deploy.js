6efe501d1b53e44d7e59fe7548157b8c7b263f16
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-29 02:41:42 +0100

    commit deploy nft hardhat solidity smart contract to blockchain

---
+++ deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+  const MyContract = await hre.ethers.getContractFactory("MyContract");
+
+  const myContractDeployed = await MyContract.deploy("MyContractName", "MCNS");
+
+  await myContractDeployed.deployed();
+
+  console.log("Deployed MyContract to: ", myContractDeployed.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
